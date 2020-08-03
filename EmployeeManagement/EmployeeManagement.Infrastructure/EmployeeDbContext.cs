2cef6ec70c2fae613c293c749c3e9182d204b67d
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-08-03 12:09:27 +0100

    Adding database context class

---
+++ EmployeeManagement/EmployeeManagement.Infrastructure/EmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeManagement.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Infrastructure
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
