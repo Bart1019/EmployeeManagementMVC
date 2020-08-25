767fd7a766f2235cb287c4dce5c8a3d5c026f2ec
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-08-25 19:21:30 +0100

    Adding possibility to editing the role

---
+++ EmployeeManagement/EmployeeManagement.Application/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using EmployeeManagement.Domain.Models;
+
+namespace EmployeeManagement.Application.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+
+        [Display(Name = "Role Id")]
+        public string Id { get; set; }
+
+        [Display(Name = "Role Name")]
+        [Required(ErrorMessage = "The Role Name is required")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}
