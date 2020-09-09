6306265a6e02b7f282e2535556f5a71be19608e7
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-09-09 21:42:17 +0100

    Adding possibility to reset password

---
+++ EmployeeManagement/EmployeeManagement.Application/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EmployeeManagement.Application.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress] 
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmed Password")]
+        [Compare("Password", ErrorMessage = "Password and confirmed password must match")]
+        public string ConfirmedPassword { get; set; }
+
+        public string Token { get; set; }
+    }
+}
