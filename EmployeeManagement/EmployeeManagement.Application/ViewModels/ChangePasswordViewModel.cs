34fc2b9c2e5a40de56ca3d432e2a619493e73df6
Bartłomiej Runowski <bartlomiej.runowski@gmail.com>
2020-09-10 12:36:24 +0100

    Adding possibility to change password

---
+++ EmployeeManagement/EmployeeManagement.Application/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EmployeeManagement.Application.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [Display(Name = "Current Password")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [Display(Name = "New Password")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Display(Name = "Confirm New Password")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "New Password and confirmed password must match")]
+        public string ConfirmedNewPassword { get; set; }
+    }
+}
